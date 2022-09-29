c1f2fb6483a71b7fb6837d7e633c3e81ccba3368
KarinaC <karinacasasj@gmail.com>
2022-09-29 02:02:04 -0300

    agregue el ir arriba,modifique colores y agrege descargables

---
+++ js/script.js
@@ -0,0 +1,21 @@
+/*------ir arriba---------*/
+document.querySelector('.icono')
+.addEventListener('click',() =>{
+window.scrollTo({
+ top:0,
+ behavior:'smooth'
+});
+});
+
+/*seguro que desea salir*/
+function salir(){
+    var respuesta = confirm('¿Seguro que desea abandonar esta pagina?');
+    if (respuesta == true)
+    {
+        return true;
+    }
+    else
+    {
+     return false;
+    }
+}
